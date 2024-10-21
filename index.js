0d7301eebe0fff15690e65fd03e7c6355ed578a3
salamus <salamus@yandex.com>
2024-10-21 10:17:20 +0300

    0/0/0/

--- index.js
+++ index.js
@@ -365,6 +365,6 @@ app.put('/:username/basket/:index', async (req, res) => {
 
 
 const PORT = process.env.PORT || 4444;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
